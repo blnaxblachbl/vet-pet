fd6c2456dbe565247729aa5a16167aa4638a6b84
Tit Hardwood <blnaxblachbl@gmail.com>
2022-07-29 16:12:18 +0900

    change pass

--- admin/src/pages/home.js
+++ admin/src/pages/home.js
@@ -5,8 +5,10 @@ import {
     Button as AntButton,
     message,
 } from 'antd'
+import { useMutation } from "@apollo/client"
 
 import { Top } from '../components'
+import { CHANGE_PASSWORD_ADMIN } from '../gqls'
 
 const Button = styled(AntButton)`
 
@@ -32,10 +34,26 @@ const Home = () => {
 
     const [form] = Form.useForm()
 
+    const [chnagePasss, { loading }] = useMutation(CHANGE_PASSWORD_ADMIN, {
+        onCompleted: () => {
+            message.success("Пароль успешно изменен")
+            form.resetFields()
+        },
+        onError: e => { }
+    })
+
     const handleSubmitForm = ({ password, confirmPassword }) => {
         if (password !== confirmPassword) {
             return message.error("Подтвердите пароль")
         }
+        chnagePasss({
+            variables: {
+                data: {
+                    password,
+                    confirmPassword
+                }
+            }
+        })
     }
 
     return (
@@ -60,7 +78,7 @@ const Home = () => {
                             placeholder="Введите новый пароль еще раз"
                         />
                     </Form.Item>
-                    <Button loading={false} type="primary" htmlType="submit">
+                    <Button loading={loading} type="primary" htmlType="submit">
                         Измененить
                     </Button>
                 </Form>
